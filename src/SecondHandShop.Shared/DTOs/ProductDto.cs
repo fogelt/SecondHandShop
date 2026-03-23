c9126110816b82678abd3e79cf53dff60f7e0c5a
fogelt <fogeltt@gmail.com>
2026-03-23 20:43:56 +0100

    Added products repo and endpoints

--- src/SecondHandShop.Shared/DTOs/ProductDto.cs
+++ src/SecondHandShop.Shared/DTOs/ProductDto.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace SecondHandShop.Shared.DTOs;
 
-namespace SecondHandShop.Shared.DTOs
+public record ProductDto
 {
-    internal class ProductDto
-    {
-        public string Name { get; set; } = string.Empty!;
-        public string Description { get; set; } = string.Empty!;
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; } = string.Empty!;
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
 }
